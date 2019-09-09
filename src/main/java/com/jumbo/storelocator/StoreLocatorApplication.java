82f0aeb7bd7c37bdcc814f7a9655a70171e542a3
tobi.oladimeji <tobi.oladimeji@interswitchgroup.com>
2019-09-09 19:57:25 +0100

    Initial project commit

---
+++ src/main/java/com/jumbo/storelocator/StoreLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.jumbo.storelocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreLocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumbo/storelocator/StoreLocatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jumbo.storelocator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoreLocatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
