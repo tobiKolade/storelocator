c325bf82d9e354ca1dea6d865365bb11f919c2ec
Tobi Oladimeji <toladimeji@microsoft.com>
2022-11-25 13:16:28 +0000

    delete file with issues

--- store-locator-backend/src/main/java/com/jumbo/storelocator/service/CodeScanErrorsToBeDeleted.java
+++
@@ -1,7 +0,0 @@
-package com.jumbo.storelocator.service;
-
-public class CodeScanErrorsToBeDeleted {
-    public void unusedLocalVariables(String var1, int var2) {
-        int another = 5;
-    }
-}
