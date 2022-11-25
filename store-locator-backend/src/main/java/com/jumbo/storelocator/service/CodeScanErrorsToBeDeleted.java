90873595a987dd4f2a96ded0b0bed77669dace8f
Tobi Oladimeji <toladimeji@microsoft.com>
2022-11-25 13:01:48 +0000

    Trigger new code scan issues with pull request

---
+++ store-locator-backend/src/main/java/com/jumbo/storelocator/service/CodeScanErrorsToBeDeleted.java
@@ -0,0 +1,7 @@
+package com.jumbo.storelocator.service;
+
+public class CodeScanErrorsToBeDeleted {
+    public void unusedLocalVariables(String var1, int var2) {
+        int another = 5;
+    }
+}
